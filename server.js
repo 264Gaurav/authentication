d086e68947ebe982bb632ae60dc79514359eb5c7
Gaurav Singh <gauravsingh264209@gmail.com>
2024-07-18 02:10:40 +0530

    serverChange

--- server.js
+++ server.js
@@ -5,8 +5,18 @@ const mainRoutes=require('./routes');
 const database=require('./database');
 const morgan=require('morgan');
 const cookieParser = require('cookie-parser'); // Import cookie-parser
+const cors=require('cors');
 
 const app=express();
+
+// Use the cors middleware to allow requests from any origin
+app.use(cors({
+    origin: '*', // Allow requests from any origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allow these HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allow these headers
+    credentials: true // Allow credentials (e.g., cookies)
+}));
+
 app.use(bodyParser.json());
 app.use(cookieParser());
 const PORT=process.env.PORT || 3222;
